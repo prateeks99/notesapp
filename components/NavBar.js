0598fa1bf80157d7187530bc34881a23e5bf626a
Prateek Saxena <44098524+prateeks99@users.noreply.github.com>
2020-07-26 06:23:46 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,14 +2,19 @@ import Link from 'next/link';
 import { Button } from 'semantic-ui-react';
 
 const Navbar  = () => (
+    <div>
     <nav className="navbar">
         <Link href="/">
-            <a className="navbar-brand">Notes-App</a>
+            <a className="navbar-brand">Post-it!</a>
          </Link>
         <Link href="/new">
             <Button className="create">Add Note</Button>
         </Link>
     </nav>
+    <footer className = "footer">
+    <p>Copyright ⓒ Prateek Saxena </p>
+    </footer>
+    </div>
 )
 
 export default Navbar;
